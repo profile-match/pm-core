90903d6539aa7b7b408600ff7505adbd0b9b36ac
Antoine <antoine.robert03@outlook.fr>
2016-12-10 23:32:56 +0100

    refactoring du package de config rest

--- src/main/java/org/profilematch/pmcore/config/ApplicationConfig.java
+++ src/main/java/org/profilematch/pmcore/config/ApplicationConfig.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.netbeans.rest.application.config;
+package org.profilematch.pmcore.config;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
