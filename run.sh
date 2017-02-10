fb05651a686eb336da6c1b9bab472f4718c0fdd1
Geoffrey-Dere <geoffrey.deremetz1@etu.univ-lorraine.fr>
2017-02-10 14:28:51 +0100

    test

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ mvn clean
 mvn package
 mvn wildfly-swarm:package
 cd target/
-
+dfdfdfd
 echo "Lmfiroot" >> password.txt
 java -Dswarm.ds.connection.url="jdbc:mysql://localhost:3306/IMP?useSSL=false" -jar pm-core-swarm.jar
