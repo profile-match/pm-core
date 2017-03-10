c1ec245764fc32e48467f5b75e1ecf2db5b04669
Antoine Robert <antoine.robert03@outlook.fr>
2017-03-10 09:56:29 +0100

    Ajout service rest pour le matching

--- src/main/java/org/profilematch/pmcore/rest/MatchingRest.java
+++ src/main/java/org/profilematch/pmcore/rest/MatchingRest.java
@@ -19,18 +19,17 @@ public class MatchingRest {
     private Matcher M;
 
     @GET
-    @Path("get/")
-    @Produces("application/json")
-    public Response GetCandidat() {
-
-        return Response.ok(M.match(1L,0,10)).build();
-    }
-
-    @GET
-    @Path("get/{idDossier}/{idCandidat}")
+    @Path("dossierCandidat/{idDossier}/{idCandidat}")
     @Produces("application/pdf")
     public Response getCV(@PathParam("idDossier") Long idDossier, @PathParam("idCandidat") Long idCandidat) {
         return Response.ok(M.matchCandidat(idDossier, idCandidat)).build();
+    }
+
+    @GET
+    @Path("dossier/{idDossier}/{borneInf}/{borneSup}")
+    @Produces("application/pdf")
+    public Response getCV(@PathParam("idDossier") Long idDossier,@PathParam("borneInf") int borneInf,@PathParam("borneSup") int borneSup) {
+        return Response.ok(M.match(idDossier, borneInf, borneSup)).build();
     }
 
 }
