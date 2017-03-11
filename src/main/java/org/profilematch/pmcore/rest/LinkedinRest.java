ccecf601ade459412773215b401128c59d91b8a0
Franck54730 <franck_audiger@hotmail.com>
2017-03-11 15:58:36 +0100

    ajout web service pour la connexion linkedin

--- src/main/java/org/profilematch/pmcore/rest/LinkedinRest.java
+++ src/main/java/org/profilematch/pmcore/rest/LinkedinRest.java
@@ -105,17 +105,85 @@ public class LinkedinRest {
                             Candidat cand = new Candidat();
                             cand.setEmail(email);
                             ce.registerUser(cand);
+                            user.setType("candidat");
                             user.setId(cand.getId());
                         }else{
                             Recruteur rec = new Recruteur();
                             rec.setEmail(email);
                             re.registerUser(rec);
+                            user.setType("recruteur");
                             user.setId(rec.getId());
                         }
                         ue.inscrireUtilisateur(user);
                         rep = ""+user.getId();
                     }else{
                         rep = ""+u.getId();
+                    }
+                }
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Response.ok(rep).build();
+    }
+    
+    @GET
+    @Path("connexion/{code}/{state}")
+    @Consumes("text/plain")
+    @Produces("application/json")
+    public Response connexionLinkedin(@PathParam("code") String code, @PathParam("state") String state){
+        String rep = "{\"id\":\"-1\", \"genre\":\"undefined\"}";
+        String front = "http%3A%2F%2Flocalhost%3A4200%2F";
+        if(IO.getProd()){
+            front = "https%3A%2F%2Fimp.bober.ovh%2F";
+        }
+        String url = "https://www.linkedin.com/oauth/v2/accessToken?" +
+            "grant_type=authorization_code&" +
+            "code=" + code + "&" +
+            "redirect_uri="+front+"connexion-linkedin&" +
+            "client_id=7868doeuipinun&" +
+            "client_secret=2HdM9JPydxMUsGWH";
+        try {
+            ClientRequest request = new ClientRequest(url);
+            request.accept("application/json");
+            ClientResponse<String> response = request.get(String.class);
+            if (response.getStatus() == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new ByteArrayInputStream(response.getEntity().getBytes())));
+                String output;
+                StringBuilder sbRep = new StringBuilder();
+                while ((output = br.readLine()) != null) {
+                    sbRep.append(output);
+                }
+                
+                JSONObject obj = new JSONObject(sbRep.toString());
+                String token = obj.getString("access_token");
+                url = "https://api.linkedin.com/v1/people/~:(email-address)?format=json";
+                
+                request = new ClientRequest(url);
+                request.accept("application/json");
+                response = request.header("Authorization", "Bearer "+token).get(String.class);
+                
+                if (response.getStatus() == 200) {
+                    br = new BufferedReader(new InputStreamReader(
+                        new ByteArrayInputStream(response.getEntity().getBytes())));
+                    sbRep = new StringBuilder();
+                    while ((output = br.readLine()) != null) {
+                        sbRep.append(output);
+                    }
+                    JSONObject obj2 = new JSONObject(sbRep.toString());
+                    String email = obj2.getString("emailAddress");
+                    Utilisateur u = ue.getUtilisateurByEmail(email);
+                    System.out.println(u.getId());
+                    if(u.getId() != -1){
+                        rep = "{"
+                            + "\"id\":\""+u.getId()+"\","
+                            + "\"genre\":\""+u.getType()+"\""
+                            + "}";
                     }
                 }
             }
